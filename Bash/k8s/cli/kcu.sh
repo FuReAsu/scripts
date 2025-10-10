6ae1092a353b4227c93bd894043c46588f33779d
Hein Htet Zaw <maintain1346@gmail.com>
2025-10-10 13:19:46 +0630

    added kcu and reshaped dir structure for k8s

---
+++ Bash/k8s/cli/kcu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+declare -a contexts=($(kubectl config get-contexts -o name))
+
+
+while true; do
+	echo "Choose a context to use..."
+	i=1
+	for context in ${contexts[@]}; do
+		echo "#$i -> $context"
+		i=$(($i+1))
+	done
+
+	read -p "#? -> " option
+
+	if [[ "$option" =~ ^([0-9]{1,2})$ ]] && [[ $option -le ${#contexts[@]} ]]; then
+		break
+	else
+		echo "invalid input"
+	fi
+	
+done
+
+option=$(($option-1))
+
+context_to_use="${contexts[$option]}"
+
+echo "Using kubectl context $context_to_use"
+
+kubectl config use-context $context_to_use
