19190dd37c7db13d8f538e6d0c3a77fe1739757a
Hein Htet Zaw <maintain1346@gmail.com>
2025-11-12 16:36:29 +0630

    improved delete-jobs and delete-pods scripts

--- Bash/k8s/cli/delete-jobs.sh
+++ Bash/k8s/cli/delete-jobs.sh
@@ -1,5 +1,20 @@
-declare -a namespaces=($(kubectl get ns | awk '{print $1}')) 
+read -p "Please input the namespace: " namespace
+
+if [ "$namespace" = "all" ]; then
+	read -p "Selecting all namspaces please confirm by pressing any key " continue
+	declare -a namespaces=($(kubectl get ns --no-headers | awk '{print $1}')) 
+else
+	declare -a namespaces=($namespace)
+fi
+
 for namespace in "${namespaces[@]}"; do 
 	echo $namespace
-	kubectl get jobs -n $namespace | grep -E "Error" | awk '{print $1}' | xargs kubectl delete jobs -n $namespace 
+	declare -a jobs=($(kubectl get jobs -n $namespace --no-headers | grep -E "Error|Pending|Running" | awk '{print $1}'))
+	if [ "${#jobs[@]}" -eq 0 ]; then
+		echo "No faulty jobs can be found..."
+	else
+		for job in "${jobs[@]}"; do
+			kubectl delete job -n $namespace $job
+		done
+	fi
 done
--- Bash/k8s/cli/delete-pods.sh
+++ Bash/k8s/cli/delete-pods.sh
@@ -1,5 +1,20 @@
-declare -a namespaces=($(kubectl get ns | awk '{print $1}')) 
+read -p "Please input the namespace: " namespace
+
+if [ "$namespace" = "all" ]; then
+	read -p "Selecting all namspaces please confirm by pressing any key " continue
+	declare -a namespaces=($(kubectl get ns --no-headers | awk '{print $1}')) 
+else
+	declare -a namespaces=($namespace)
+fi
+
 for namespace in "${namespaces[@]}"; do 
 	echo $namespace
-	kubectl get pods -n $namespace | grep -E "Terminating|Err|ImagePullBackOff" | awk '{print $1}' | xargs kubectl delete pods -n $namespace --force 
+	declare -a pods=($(kubectl get pods -n $namespace --no-headers | grep -E "Terminating|Err|ImagePullBackOff" | awk '{print $1}'))
+	if [ "${#pods[@]}" -eq 0 ]; then
+		echo "No faulty pods can be found..."
+	else
+		for pod in "${pods[@]}"; do
+			kubectl delete pod -n $namespace $pod
+		done
+	fi
 done
